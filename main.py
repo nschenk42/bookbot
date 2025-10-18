e5ba7b31fc66a2ab545bd4b34144309842746438
nschenk42 <n.schenk@gmail.com>
2025-10-18 20:40:42 +0200

    working main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    the_text = get_book_text(path_to_file)
+    print(the_text)
+main() 
+
