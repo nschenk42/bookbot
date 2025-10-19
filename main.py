4964a145f5aae8befdf7ffc2b0becebe19bdda12
nschenk42 <n.schenk@gmail.com>
2025-10-19 14:05:00 +0200

    testing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words
+print(get_num_words, get_num_words.stats)
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -7,10 +10,10 @@ def main():
     path_to_file = "books/frankenstein.txt"
     the_text = get_book_text(path_to_file)
     #print(the_text)
+    num = get_num_words(the_text)
+    print(f"Found {num} total words")
 
-#starting a counter here
-    splitter = the_text.split()
-    splitcounter = len(splitter)
-    print(f"Found {splitcounter} total words")
-main() 
+if __name__ == "__main__":
+    main()
+
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words():
+    splitter = the_text.split()
+    splitcounter = len(splitter)
+    print(f"Found {splitcounter} total words")
