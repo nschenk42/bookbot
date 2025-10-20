faaa526c69d5074d819f708e0941ef8c42efc25c
nschenk42 <n.schenk@gmail.com>
2025-10-20 20:06:52 +0200

    taken files from solution

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from stats import get_num_words
-print(get_num_words, get_num_words.stats)
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
 
 def main():
-    path_to_file = "books/frankenstein.txt"
-    the_text = get_book_text(path_to_file)
-    #print(the_text)
-    num = get_num_words(the_text)
-    print(f"Found {num} total words")
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
 
-if __name__ == "__main__":
-    main()
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
 
 
